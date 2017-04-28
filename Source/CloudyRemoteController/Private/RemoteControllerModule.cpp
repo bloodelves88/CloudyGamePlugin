627efb5990a1b56a2273872c74ebec22d9f7c506
Joel <blood_elves88@yahoo.com.sg>
2017-04-28 16:13:07 +0800

    Changed hardcoded implementation of the creation of files. Uses a C++ vector now

--- Source/CloudyRemoteController/Private/RemoteControllerModule.cpp
+++ Source/CloudyRemoteController/Private/RemoteControllerModule.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <ctime>
+#include <vector>
 
 #define CLOUDYGAME_REMOTE_CONTROLLER_SERVER_DEFAULT_ENDPOINT FIPv4Endpoint(FIPv4Address(0, 0, 0, 0), 55555)
 #define BUFSIZE 512
@@ -14,25 +16,17 @@ DEFINE_LOG_CATEGORY(RemoteControllerLog)
 UGameInstance* GameInstance;
 TArray<UWorld*> WorldArray;
 
-HANDLE pipeInstance;
-BOOL isConnected;
-BOOL fSuccess;
-unsigned long cbRead;
-int counter;
-TCHAR buf[1];
-LPTSTR lpvMessage = TEXT("Default message from server.\n\0");
-std::ofstream TestFile0;
-std::ofstream TestFile1;
-std::ofstream TestFile2;
-std::ofstream TestFile3;
+std::vector<std::ofstream> PlayerInputFileArray;
+time_t timeLastWrittenMouseMovement = std::time(0);
+time_t timeLastWrittenKeyboardInput = std::time(0);
 
 void RemoteControllerModule::StartupModule()
 {
 	UE_LOG(RemoteControllerLog, Warning, TEXT("CloudyGame: RemoteController Module Starting"));
 
-    const FString& SocketName = "RemoteControllerSocket";
-    const FString& IPAddress = "0.0.0.0";
-    const int32 Port = 55555;
+	const FString& SocketName = "RemoteControllerSocket";
+	const FString& IPAddress = "0.0.0.0";
+	const int32 Port = 55555;
 
 	//Retrieve GEngine->CNumOfPlayers setting from DefaultGame.ini
 	FString NumberOfPlayersString;
@@ -44,64 +38,13 @@ void RemoteControllerModule::StartupModule()
 	);
 
 	WorldArray.Init(NULL, FCString::Atoi(*NumberOfPlayersString));
-	//TestFileArray.Init(nullptr, FCString::Atoi(*NumberOfPlayersString));
-
-    InitializeRemoteServer(SocketName, IPAddress, Port);
-
-	//LPTSTR lpszPipename = TEXT("\\\\.\\pipe\\playerInputPipe");
-	//pipeInstance = CreateNamedPipe(lpszPipename, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_NOWAIT, 
-	//							   PIPE_UNLIMITED_INSTANCES, BUFSIZE, BUFSIZE, 0, NULL);
-	////pipeInstance = CreateFile(TEXT("test.txt"), GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
-	//
-	//if (pipeInstance == INVALID_HANDLE_VALUE)
-	//{
-	//	UE_LOG(RemoteControllerLog, Warning, TEXT("CreateNamedPipe failed with %d"), GetLastError());
-	//}
-	//isConnected = 0;
-	//fSuccess = 0;
-
 	
-	if (FCString::Atoi(*NumberOfPlayersString) <= 1)
+	InitializeRemoteServer(SocketName, IPAddress, Port);
+	
+	for (int i = 0; i < FCString::Atoi(*NumberOfPlayersString); i++)
 	{
-		std::ostringstream oss;
-		oss << "test" << 0 << ".txt";
-		TestFile0.open(oss.str(), std::ios::trunc);
-	}
-	else if (FCString::Atoi(*NumberOfPlayersString) <= 2)
-	{
-		std::ostringstream oss;
-		oss << "test" << 0 << ".txt";
-		TestFile0.open(oss.str(), std::ios::trunc);
-		oss.str("");
-		oss << "test" << 1 << ".txt";
-		TestFile1.open(oss.str(), std::ios::trunc);
-	}
-	else if (FCString::Atoi(*NumberOfPlayersString) <= 3)
-	{
-		std::ostringstream oss;
-		oss << "test" << 0 << ".txt";
-		TestFile0.open(oss.str(), std::ios::trunc);
-		oss.str("");
-		oss << "test" << 1 << ".txt";
-		TestFile1.open(oss.str(), std::ios::trunc);
-		oss.str("");
-		oss << "test" << 2 << ".txt";
-		TestFile2.open(oss.str(), std::ios::trunc);
-	}
-	else if (FCString::Atoi(*NumberOfPlayersString) <= 4)
-	{
-		std::ostringstream oss;
-		oss << "test" << 0 << ".txt";
-		TestFile0.open(oss.str(), std::ios::trunc);
-		oss.str("");
-		oss << "test" << 1 << ".txt";
-		TestFile1.open(oss.str(), std::ios::trunc);
-		oss.str("");
-		oss << "test" << 2 << ".txt";
-		TestFile2.open(oss.str(), std::ios::trunc);
-		oss.str("");
-		oss << "test" << 3 << ".txt";
-		TestFile3.open(oss.str(), std::ios::trunc);
+		std::string fileName = "test" + std::to_string(i) + ".txt";
+		PlayerInputFileArray.emplace_back(std::ofstream{ fileName });
 	}
 }
 
@@ -149,42 +92,13 @@ void RemoteControllerModule::ProcessKeyboardInput(const FArrayReaderPtr& Data)
 	// Mouse buttons
 	if (Chunk.KeyCode == 1 || Chunk.KeyCode == 2 || Chunk.KeyCode == 0)
 	{
-		if (Chunk.ControllerID == 0)
-		{
-			TestFile0 << 3 << std::endl;
-		}
-		else if (Chunk.ControllerID == 1)
-		{
-			TestFile1 << 3 << std::endl;
-		}
-		else if (Chunk.ControllerID == 2)
-		{
-			TestFile2 << 3 << std::endl;
-		}
-		else if (Chunk.ControllerID == 3)
-		{
-			TestFile3 << 3 << std::endl;
-		}
+		PlayerInputFileArray[Chunk.ControllerID] << 3 << std::endl;
 	}
 	// Keyboard buttons
-	else
+	else if (std::time(0) - timeLastWrittenKeyboardInput >= 1)
 	{
-		if (Chunk.ControllerID == 0)
-		{
-			TestFile0 << 2 << std::endl;
-		}
-		else if (Chunk.ControllerID == 1)
-		{
-			TestFile1 << 2 << std::endl;
-		}
-		else if (Chunk.ControllerID == 2)
-		{
-			TestFile2 << 2 << std::endl;
-		}
-		else if (Chunk.ControllerID == 3)
-		{
-			TestFile3 << 2 << std::endl;
-		}
+		timeLastWrittenKeyboardInput = std::time(0);
+		PlayerInputFileArray[Chunk.ControllerID] << 2 << std::endl;
 	}
 
 	// If world has not been loaded yet
@@ -202,8 +116,6 @@ void RemoteControllerModule::ProcessKeyboardInput(const FArrayReaderPtr& Data)
 		ie = EInputEvent::IE_Released;
 	}
 
-	
-
 	FKey key = FInputKeyManager::Get().GetKeyFromCodes(Chunk.KeyCode, Chunk.CharCode);
     if (controller != nullptr)
     {
@@ -217,21 +129,10 @@ void RemoteControllerModule::ProcessMouseInput(const FArrayReaderPtr& Data)
     FUdpRemoteControllerSegment::FMouseInputChunk Chunk;
 	*Data << Chunk;
 
-	if (Chunk.ControllerID == 0)
+	if (std::time(0) - timeLastWrittenMouseMovement >= 1)
 	{
-		TestFile0 << 2 << std::endl;
-	}
-	else if (Chunk.ControllerID == 1)
-	{
-		TestFile1 << 2 << std::endl;
-	}
-	else if (Chunk.ControllerID == 2)
-	{
-		TestFile2 << 2 << std::endl;
-	}
-	else if (Chunk.ControllerID == 3)
-	{
-		TestFile3 << 2 << std::endl;
+		timeLastWrittenMouseMovement = std::time(0);
+		PlayerInputFileArray[Chunk.ControllerID] << 2 << std::endl;
 	}
 
 	// If world has not been loaded yet
@@ -259,57 +160,6 @@ void RemoteControllerModule::HandleInputReceived(const FArrayReaderPtr& Data, co
 {
 	FUdpRemoteControllerSegment::FHeaderChunk Chunk;
 	*Data << Chunk;
-	
-	// This can be blocking until a client connects or non-blocking 
-	// However the window will not appear if this blocks.
-	// If a window does not appear, NvIFREncoder will not run.
-	//if (isConnected == 0) // false
-	//{
-	//	isConnected = ConnectNamedPipe(pipeInstance, NULL);
-	//	if (isConnected != 0) // true
-	//	{
-	//		UE_LOG(RemoteControllerLog, Warning, TEXT("Client connected to the pipe!"));
-	//	} 
-	//	else // false
-	//	{
-	//		UE_LOG(RemoteControllerLog, Warning, TEXT("ConnectNamedPipe failed with %d"), GetLastError());
-	//	}
-	//}
-
-	//std::string end = "\n\0";
-	
-	//end = std::to_string(counter) + end;
-
-	//fSuccess = WriteFile(pipeInstance,
-	//					 //lpvMessage,
-	//					 //(lstrlen(lpvMessage)) *sizeof(TCHAR),
-	//					 buf,
-	//					 //end.c_str(),
-	//					 sizeof(buf),
-	//	                 &cbRead,
-	//	                 NULL);
-	
-	//UE_LOG(RemoteControllerLog, Warning, TEXT("lpvMessage length = %d"), (lstrlen(lpvMessage) + 1) * sizeof(TCHAR));
-	
-	
-
-	// Keyboard or mouse: Chunk.SegmentType
-	// ============
-	// FUdpRemoteControllerSegment::FKeyboardInputChunk Chunk;
-	// *Data << Chunk;
-	// Keyboard: uint8 ControllerID;
-	//           int16 KeyCode;
-	//           int16 CharCode;
-	//           uint8 InputEvent;
-	// ============
-	// FUdpRemoteControllerSegment::FMouseInputChunk Chunk;
-	// *Data << Chunk;
-	// Mouse: uint8 ControllerID;
-	//        int16 XAxis;
-	//        int16 YAxis;
-
-	// A pipe can be created ONCE during startup, and all this info 
-	// can be constantly piped to NvIFREncoder.cpp to read
 	
     switch (Chunk.SegmentType)
     {
